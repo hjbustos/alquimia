4ce8aac35f23d39957c16fd481f80951bff4240b
Humberto J <hjbustos@gmail.com>
2015-06-05 23:05:18 -0300

    Modelo de Pagina de inicio

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'public#index'
+  root 'public#store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
